c8154a958c31af8d2fbb9a450f9fea9dcbc06109
Pierre Rudloff <contact@rudloff.pro>
2016-07-01 01:03:52 +0200

    Export source image

--- classes/ExportSourceCommand.php
+++ classes/ExportSourceCommand.php
@@ -28,6 +28,11 @@ class ExportSourceCommand extends ExportCommand
                 'id',
                 InputArgument::REQUIRED,
                 'Source ID'
+            )->addOption(
+                'force',
+                null,
+                InputOption::VALUE_NONE,
+                'Force reupload'
             );
     }
 
@@ -49,7 +54,8 @@ class ExportSourceCommand extends ExportCommand
         parent::setup($input, $output);
 
         $id = $input->getArgument('id');
-        $pageName = 'Source:'.$this->s->getSourceLibelle($id);
+        $origPageName = $this->s->getSourceLibelle($id);
+        $pageName = 'Source:'.$origPageName;
 
         $output->writeln('<info>Exporting "'.$pageName.'"…</info>');
 
@@ -65,6 +71,32 @@ class ExportSourceCommand extends ExportCommand
 
         //Login as bot
         $this->login('aw2mw bot');
+
+        $oldPath = 'http://www.archi-wiki.org/images/logosSources/'.$id.'_original.jpg';
+        $headers = get_headers($oldPath, 1);
+        if ($headers[0] == 'HTTP/1.1 200 OK') {
+            $filename = 'Source '.$origPageName.'.jpg';
+
+            $params = array(
+                'filename'=>$filename,
+                'token'=>$this->api->getToken('edit'),
+                'url'=>$oldPath
+            );
+            if ($input->getOption('force')) {
+                $params['ignorewarnings'] = true;
+            }
+
+            $output->writeln('<info>Exporting "File:'.$filename.'"…</info>');
+            $this->api->postRequest(
+                new Api\SimpleRequest(
+                    'upload',
+                    $params,
+                    array()
+                )
+            );
+            $html = '[[File:'.$filename.'|thumb]]'.PHP_EOL.$html;
+        }
+
         $this->savePage($pageName, $html, 'Source importée depuis Archi-Wiki');
     }
 }
