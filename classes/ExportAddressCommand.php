2c6686193f221bbf695a90bf98b03eb52ba4b06d
Pierre Rudloff <contact@rudloff.pro>
2016-07-02 17:42:05 +0200

    Display complete address

--- classes/ExportAddressCommand.php
+++ classes/ExportAddressCommand.php
@@ -197,6 +197,95 @@ class ExportAddressCommand extends ExportCommand
         if (!$isNews) {
             $intro = '{{Infobox adresse'.PHP_EOL;
 
+            $idEvenementGroupeAdresse = $this->a->getIdEvenementGroupeAdresseFromIdAdresse($address['idAdresse']);
+
+            $reqAdresseDuGroupeAdresse = "
+                SELECT ha1.idAdresse as idAdresse,ha1.numero as numero,
+                ha1.idRue as idRue, IF(ha1.idIndicatif='0','',i.nom)
+                as nomIndicatif, ha1.idQuartier as idQuartier, ha1.idSousQuartier as idSousQuartier
+                FROM historiqueAdresse ha2, historiqueAdresse ha1
+                LEFT JOIN _adresseEvenement ae ON ae.idAdresse = ha1.idAdresse
+                LEFT JOIN indicatif i ON i.idIndicatif = ha1.idIndicatif
+                WHERE ha2.idAdresse = ha1.idAdresse
+                AND ae.idEvenement ='".mysql_real_escape_string($idEvenementGroupeAdresse)."'
+
+                GROUP BY ha1.idAdresse, ha1.idHistoriqueAdresse
+                HAVING ha1.idHistoriqueAdresse = max(ha2.idHistoriqueAdresse)
+                ORDER BY ha1.numero,ha1.idRue
+            ";
+
+            $resAdresseDuGroupeAdresse = $this->a->connexionBdd->requete($reqAdresseDuGroupeAdresse);
+
+            if (mysql_num_rows($resAdresseDuGroupeAdresse)>1) {
+                $txtAdresses = '';
+                $arrayNumero = array();
+                while ($fetchAdressesGroupeAdresse = mysql_fetch_assoc($resAdresseDuGroupeAdresse)) {
+                    $isAdresseCourante = false;
+                    if ($address['idAdresse'] == $fetchAdressesGroupeAdresse['idAdresse']) {
+                        $isAdresseCourante = true;
+                    }
+
+                    if ($fetchAdressesGroupeAdresse['idRue']=='0' || $fetchAdressesGroupeAdresse['idRue']=='') {
+                        if ($fetchAdressesGroupeAdresse['idQuartier']!=''
+                            && $fetchAdressesGroupeAdresse['idQuartier']!='0'
+                        ) {
+                            $arrayNumero[$this->getIntituleAdresseFrom(
+                                $fetchAdressesGroupeAdresse['idAdresse'],
+                                'idAdresse',
+                                array('noSousQuartier'=>true,'noQuartier'=>false,'noVille'=>true)
+                            )][] =
+                                array(
+                                    'indicatif'=>$fetchAdressesGroupeAdresse['nomIndicatif'],
+                                    'numero'=>$fetchAdressesGroupeAdresse['numero'],
+                                    'isAdresseCourante'=>$isAdresseCourante
+                                );
+                        }
+
+                        if ($fetchAdressesGroupeAdresse['idSousQuartier']!=''
+                            && $fetchAdressesGroupeAdresse['idSousQuartier']!='0'
+                        ) {
+                            $arrayNumero[$this->getIntituleAdresseFrom(
+                                $fetchAdressesGroupeAdresse['idAdresse'],
+                                'idAdresse',
+                                array('noSousQuartier'=>false,'noQuartier'=>true,'noVille'=>true)
+                            )][] =
+                                array(
+                                    'indicatif'=>$fetchAdressesGroupeAdresse['nomIndicatif'],
+                                    'numero'=>$fetchAdressesGroupeAdresse['numero'],
+                                    'isAdresseCourante'=>$isAdresseCourante
+                                );
+                        }
+                    } else {
+                        $arrayNumero[$this->a->getIntituleAdresseFrom(
+                            $fetchAdressesGroupeAdresse['idRue'],
+                            'idRueWithNoNumeroAuthorized',
+                            array('noSousQuartier'=>true,'noQuartier'=>true,'noVille'=>true)
+                        )][] =
+                            array(
+                                'indicatif'=>$fetchAdressesGroupeAdresse['nomIndicatif'],
+                                'numero'=>$fetchAdressesGroupeAdresse['numero'],
+                                'isAdresseCourante'=>$isAdresseCourante
+                            );
+                    }
+                }
+
+                // affichage adresses regroupees
+                foreach ($arrayNumero as $intituleRue => $arrayInfosNumero) {
+                    foreach ($arrayInfosNumero as $indice => $infosNumero) {
+                        if ($infosNumero['numero']=='' || $infosNumero['numero']=='0') {
+                        //rien
+                        } else {
+                            $txtAdresses.=$infosNumero['numero'].$infosNumero['indicatif']."-";
+                        }
+                    }
+
+                    $txtAdresses = trim($txtAdresses, '-');
+
+                    $txtAdresses.= $intituleRue.', ';
+                }
+                $txtAdresses = trim($txtAdresses, ', ');
+            }
+
             foreach ($infobox as $i => $info) {
                 foreach ($info['people'] as $job => $name) {
                     $intro .= '|'.$job.($i + 1).' = '.$name.PHP_EOL;
@@ -217,7 +306,9 @@ class ExportAddressCommand extends ExportCommand
             $intro .= '|complément_rue = '.$address['prefixeRue'].PHP_EOL;
             $intro .= '|ville = '.$address['nomVille'].PHP_EOL;
             $intro .= '|pays = '.$address['nomPays'].PHP_EOL;
-
+            if (isset($txtAdresses)) {
+                $intro .= '|nom_complet = '.$txtAdresses.PHP_EOL;
+            }
             $intro .= '}}'.PHP_EOL.PHP_EOL;
             $sections[0] = $intro;
 
