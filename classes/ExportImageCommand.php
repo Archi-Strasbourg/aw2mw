bd62fad0ce610515497eafb4d2afbc0eae147767
Pierre Rudloff <contact@rudloff.pro>
2016-10-10 23:07:00 +0200

    Lint

--- classes/ExportImageCommand.php
+++ classes/ExportImageCommand.php
@@ -3,7 +3,6 @@
 namespace AW2MW;
 
 use Mediawiki\Api;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
