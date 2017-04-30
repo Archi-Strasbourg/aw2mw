758139d0f158ae5e2f443ab4b220e07b9f3dedc6
Pierre Rudloff <contact@rudloff.pro>
2017-04-30 02:33:58 +0200

    Lint

--- classes/ExportAddressCommand.php
+++ classes/ExportAddressCommand.php
@@ -567,7 +567,7 @@ class ExportAddressCommand extends ExportCommand
     }
 
     /**
-     * @param int $section
+     * @param int    $section
      * @param string $pageName
      */
     private function exportEventImages($id, $section, $pageName)
