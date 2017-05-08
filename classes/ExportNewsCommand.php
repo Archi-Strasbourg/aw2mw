98949d3f73f7da4d7cc64c9915f9ddfab3833a70
Pierre Rudloff <contact@rudloff.pro>
2017-05-08 16:28:49 +0200

    Add images in news

--- classes/ExportNewsCommand.php
+++ classes/ExportNewsCommand.php
@@ -33,6 +33,27 @@ class ExportNewsCommand extends ExportCommand
             );
     }
 
+    private function importImage(InputInterface $input, OutputInterface $output, $filename, $url)
+    {
+        $params = [
+            'filename' => $filename,
+            'token'    => $this->api->getToken('edit'),
+            'url'      => $url,
+        ];
+        if ($input->getOption('force')) {
+            $params['ignorewarnings'] = true;
+        }
+
+        $output->writeln('<info>Exporting "File:'.$filename.'"…</info>');
+        $this->api->postRequest(
+            new Api\SimpleRequest(
+                'upload',
+                $params,
+                []
+            )
+        );
+    }
+
     /**
      * Execute command.
      *
@@ -77,27 +98,22 @@ class ExportNewsCommand extends ExportCommand
             $filename = str_replace('.', '-', $filename);
             $filename .= '.jpg';
 
-            $params = [
-                'filename' => $filename,
-                'token'    => $this->api->getToken('edit'),
-                'url'      => 'http://www.archi-wiki.org/images/actualites/'.
-                                $news['idActualite'].'/'.$news['photoIllustration'],
-            ];
-            if ($input->getOption('force')) {
-                $params['ignorewarnings'] = true;
-            }
-
-            $output->writeln('<info>Exporting "File:'.$filename.'"…</info>');
-            $this->api->postRequest(
-                new Api\SimpleRequest(
-                    'upload',
-                    $params,
-                    []
-                )
+            $this->importImage(
+                $input,
+                $output,
+                $filename,
+                'http://www.archi-wiki.org/images/actualites/'.
+                    $news['idActualite'].'/'.$news['photoIllustration']
             );
 
             $html .= '[[Fichier:'.$filename.'|thumb]]';
         }
+
+        $news['texte'] = str_replace(
+            "###cheminImages###",
+            $this->a->getUrlImage()."actualites/".$news['idActualite']."/",
+            $news['texte']
+        );
 
         $html .= $this->convertHtml($news['texte']);
         $html = str_replace(
@@ -110,6 +126,49 @@ class ExportNewsCommand extends ExportCommand
             '[[Utilisateur:Digito/me_contacter|Fabien Romary]]',
             $html
         );
+
+        //Import images in text
+        preg_match_all(
+            '#\[\[Image:(([^\|]+)(\|.+)?)\]\]#iU',
+            $html,
+            $matches,
+            PREG_SET_ORDER
+        );
+        if (is_array($matches)) {
+            foreach ($matches as $match) {
+                $filename = 'Actualité '.stripslashes($news['titre']);
+                $filename = str_replace('/', '-', $filename);
+                $filename = str_replace('.', '-', $filename);
+                $filename .= ' - '.str_replace('%20', ' ', $match[2]);
+                preg_match(
+                    '#[\"\']([^\"\']+)('.preg_quote(str_replace('%20', ' ', $match[2])).'|'.preg_quote($match[2]).')[\"\']#iU',
+                    stripslashes($news['texte']),
+                    $imageMatches
+                );
+                if (filter_var($imageMatches[1], FILTER_VALIDATE_URL) == false) {
+                    $imageUrl = 'http://archi-wiki.org/'.$imageMatches[1].$match[2];
+                } else {
+                    $imageUrl = $imageMatches[1].$match[2];
+                }
+                $imageUrl = str_replace('https', 'http', $imageUrl);
+                try {
+                    $this->importImage(
+                        $input,
+                        $output,
+                        $filename,
+                        $imageUrl
+                    );
+                    if (isset($match[3])) {
+                        $attributes = $match[3];
+                    } else {
+                        $attributes = '';
+                    }
+                    $html = str_replace($match[0], '[[Image:'.$filename.$attributes.']]', $html);
+                } catch (Api\UsageException $e) {
+                    $output->writeln("<error>Couldn't import image ".$match[2].':'.PHP_EOL.$e->getMessage().'</error>');
+                }
+            }
+        }
 
         $this->pageSaver->savePage(
             $pageName,
