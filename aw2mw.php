efc07d0ba8137c20e5a5a8823c31be9b04f7f715
Pierre Rudloff <contact@rudloff.pro>
2017-05-08 13:13:32 +0200

    New export:route:all command

--- aw2mw.php
+++ aw2mw.php
@@ -3,6 +3,7 @@
 require_once 'vendor/autoload.php';
 use AW2MW\ExportAddressCommand;
 use AW2MW\ExportAllCommand;
+use AW2MW\ExportAllRouteCommand;
 use AW2MW\ExportAllSourceCommand;
 use AW2MW\ExportAllStreetCommand;
 use AW2MW\ExportAllUserCommand;
@@ -21,6 +22,7 @@ $application->add(new ExportAllCommand());
 $application->add(new ExportAllStreetCommand());
 $application->add(new ExportAllSourceCommand());
 $application->add(new ExportAllUserCommand());
+$application->add(new ExportAllRouteCommand());
 $application->add(new ExportAddressCommand());
 $application->add(new ExportSourceCommand());
 $application->add(new ExportImageCommand());
