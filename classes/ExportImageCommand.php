b32e12e77add3917b8c7120ab2144b9f5416b9e4
Pierre Rudloff <contact@rudloff.pro>
2017-08-28 16:01:58 +0200

    Lint

--- classes/ExportImageCommand.php
+++ classes/ExportImageCommand.php
@@ -75,7 +75,7 @@ class ExportImageCommand extends ExportCommand
                 'filename' => $filename,
                 'token'    => $this->api->getToken('edit'),
                 'url'      => $oldPath,
-                'async'    => true
+                'async'    => true,
             ];
             if ($this->input->getOption('force')) {
                 $params['ignorewarnings'] = true;
